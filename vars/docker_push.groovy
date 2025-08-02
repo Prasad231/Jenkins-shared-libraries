fd511bed1a47781c3814af52fa56753a12bee921
Prasad231 <153746987+Prasad231@users.noreply.github.com>
2025-08-02 20:27:27 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,7 +2,7 @@ def call(String DOCKER_USERNAME, String imagename, String imagetag  ){
   echo "Pushing built image to DockerHub"
   withCredentials([usernamePassword(credentialsId: "Docker_Hub_Cred",usernameVariable: "DOCKER_USERNAME", passwordVariable: "DOCKER_PASSWORD")]){
   sh "docker login -u ${env.DOCKER_USERNAME} -p ${env.DOCKER_PASSWORD}"
-  sh "docker image tag imagename:imagetag ${env.DOCKER_USERNAME}/imagename:imagetag"  
+  sh "docker image tag ${imagename}:${imagetag} ${env.DOCKER_USERNAME}/${imagename}:${imagetag}"  
   sh "docker push ${env.DOCKER_USERNAME}/${imagename}:${imagetag}"  
   echo "Pushing built image to DockerHub competed "
 }
