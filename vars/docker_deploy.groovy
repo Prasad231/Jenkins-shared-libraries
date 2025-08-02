e0defc30756d9e40140cb5ac660a53363a9b7034
Prasad231 <153746987+Prasad231@users.noreply.github.com>
2025-08-02 19:06:28 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,4 +1,5 @@
-def call(Srting imagename, String imagetag){
+def call(String imagename, String imagetag){
 echo " Deploying image container using docker "
 sh " docker run -d -p 8000:8000 ${imagename}:${imagetag} "
+echo " Deploying image container using docker completed"
 }
