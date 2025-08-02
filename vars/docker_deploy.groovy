239480c6044dad7ac09adbd20c8569219ecf955f
Prasad231 <153746987+Prasad231@users.noreply.github.com>
2025-08-02 19:04:33 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,5 +1,4 @@
-def call(){
+def call(Srting imagename, String imagetag){
 echo " Deploying image container using docker "
-sh " docker run -d -p 8000:8000 notesapp:latest"
-
+sh " docker run -d -p 8000:8000 ${imagename}:${imagetag} "
 }
